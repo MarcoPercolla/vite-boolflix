b46f1f0bb631a9ebd73e84fec775e192fcf77be2
ReDMallard <marco@titori.it>
2023-12-04 23:50:53 +0100

    eseguo chiamata axios

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<script>
+import axios from 'axios';
+import { store } from "./store.js"
+
+export default {
+  data() {
+    return {
+      store,
+    }
+  },
+  methods: {
+    getMovies() {
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/search/movie',
+        params: { query: 'future', include_adult: 'false', language: 'en-US', page: '1' },
+        headers: {
+          accept: 'application/json',
+          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmI2Zjg2ZjUyZWUzMjgxODMxNDcwODIyODFhMTU0MyIsInN1YiI6IjY1NmRmMGEzMDg1OWI0MDBhZDM5ZjhjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xe2Vb9urprdUXsWvCYsJcD5yKZmcaCCZ5LUIhgb5qyc'
+        },
+
+      };
+
+      axios
+        .request(options)
+        .then(function (response) {
+          console.log(response.data.results);
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    }
+  },
+  mounted() {
+    this.getMovies()
+  }
+}
+</script>
+
+<template></template>
+
+<style scoped></style>
+
+
+
