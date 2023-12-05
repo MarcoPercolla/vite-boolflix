d20ff12f363c52ece7c2317602e241ee5023617b
ReDMallard <marco@titori.it>
2023-12-05 04:24:35 +0100

    preparazione per controllo lingue

--- src/components/AppSearchMovie.vue
+++ src/components/AppSearchMovie.vue
@@ -20,7 +20,7 @@ export default {
 </script>
 <template>
     <input type="text" placeholder="cerca..." v-model="store.searchString" @keyup="getInput">
-    <select name="lng" id="lng">
+    <select name="lng" id="lng" v-model="store.currentLng">
         <option value="all">all</option>
         <option value="en">En</option>
         <option value="it">it</option>
