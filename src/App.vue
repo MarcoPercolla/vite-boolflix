43b52f20bd7325c323d92b50c115acb9e24ece10
ReDMallard <marco@titori.it>
2023-12-05 02:38:17 +0100

    emit integrato

--- src/App.vue
+++ src/App.vue
@@ -17,26 +17,54 @@ export default {
   },
   methods: {
     getMovies() {
-      const options = {
-        method: 'GET',
-        url: 'https://api.themoviedb.org/3/search/movie',
-        params: { query: 'future', include_adult: 'false', language: 'en-US', page: '1' },
-        headers: {
-          accept: 'application/json',
-          Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmI2Zjg2ZjUyZWUzMjgxODMxNDcwODIyODFhMTU0MyIsInN1YiI6IjY1NmRmMGEzMDg1OWI0MDBhZDM5ZjhjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xe2Vb9urprdUXsWvCYsJcD5yKZmcaCCZ5LUIhgb5qyc'
-        },
 
-      };
+      if (this.store.searchString) {
+        const options = {
+          method: 'GET',
+          url: 'https://api.themoviedb.org/3/search/movie',
+          params: { query: `${this.store.searchString}`, include_adult: 'false', language: 'en-US', page: '1' },
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmI2Zjg2ZjUyZWUzMjgxODMxNDcwODIyODFhMTU0MyIsInN1YiI6IjY1NmRmMGEzMDg1OWI0MDBhZDM5ZjhjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xe2Vb9urprdUXsWvCYsJcD5yKZmcaCCZ5LUIhgb5qyc'
+          },
 
-      axios
-        .request(options)
-        .then(function (response) {
-          console.log(response.data.results);
-          store.moviesList = response.data.results;
-        })
-        .catch(function (error) {
-          console.error(error);
-        });
+        };
+
+        axios
+          .request(options)
+          .then(function (response) {
+            console.log(response.data.results);
+            store.moviesList = response.data.results;
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+
+
+      } else {
+        const options = {
+          method: 'GET',
+          url: 'https://api.themoviedb.org/3/search/movie',
+          params: { query: `transformers`, include_adult: 'false', language: 'en-US', page: '1' },
+          headers: {
+            accept: 'application/json',
+            Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmI2Zjg2ZjUyZWUzMjgxODMxNDcwODIyODFhMTU0MyIsInN1YiI6IjY1NmRmMGEzMDg1OWI0MDBhZDM5ZjhjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xe2Vb9urprdUXsWvCYsJcD5yKZmcaCCZ5LUIhgb5qyc'
+          },
+
+        };
+
+        axios
+          .request(options)
+          .then(function (response) {
+            console.log(response.data.results);
+            store.moviesList = response.data.results;
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+
+      }
+
     }
   },
   mounted() {
@@ -47,7 +75,7 @@ export default {
 
 <template>
   <main>
-    <AppSearchMovie />
+    <AppSearchMovie @searchFilm="getMovies" />
     <section class="container">
       <AppMovieCard v-for="movie in store.moviesList" :film="movie" />
     </section>
--- src/components/AppSearchMovie.vue
+++ src/components/AppSearchMovie.vue
@@ -1,9 +1,10 @@
 <script>
 import { store } from "../store.js"
-import axios from 'axios';
+
 
 export default {
     name: "AppSearchMovie",
+    emits: ["searchFilm"],
     data() {
         return {
             store
@@ -11,32 +12,9 @@ export default {
     },
     methods: {
         getInput() {
-            console.log(this.store.searchString);
-            this.getMovies()
-
+            this.$emit("searchFilm");
         },
-        getMovies() {
-            const options = {
-                method: 'GET',
-                url: 'https://api.themoviedb.org/3/search/movie',
-                params: { query: `${this.store.searchString}`, include_adult: 'false', language: 'en-US', page: '1' },
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkZmI2Zjg2ZjUyZWUzMjgxODMxNDcwODIyODFhMTU0MyIsInN1YiI6IjY1NmRmMGEzMDg1OWI0MDBhZDM5ZjhjZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.xe2Vb9urprdUXsWvCYsJcD5yKZmcaCCZ5LUIhgb5qyc'
-                },
 
-            };
-
-            axios
-                .request(options)
-                .then(function (response) {
-                    console.log(response.data.results);
-                    store.moviesList = response.data.results;
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
-        }
     }
 }
 </script>
